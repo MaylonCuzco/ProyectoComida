2d13333e21cddd32751e0f8bb4b4ff4be37e1812
Jessie Contreras <50623179+JessieContreras@users.noreply.github.com>
2023-06-09 04:28:31 -0500

    Update editar.blade.php
    Se le dio formato

--- resources/views/productos/editar.blade.php
+++ resources/views/productos/editar.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-
     <div class="d-flex justify-content-center m-5">
         <div class="card p-5">
             <h1>Editar producto</h1><br>
@@ -41,7 +40,6 @@
                     <input type="text" class="form-control" value="{{ $producto->precio_unitario }}"
                         name="precio_unitario" step="0.01" required><br><br>
                 </div>
-
                 <label for="categoria">Categoría:</label>
                 <select class="form-select" name="categoria" required>
                     @if ($producto->categoria == 'comida')
